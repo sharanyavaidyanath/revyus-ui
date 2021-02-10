c07e6db7ba172a68b892e96ce3478e27565480f4
Sharanya <sharuv89@gmail.com>
2021-02-10 13:48:52 +0530

    Bootstrapping project

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Revyus</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hello</div>
+    </div>
+  );
+}
